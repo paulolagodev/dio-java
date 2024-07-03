b36f1d7500ce60ed165fc610809af5724e068240
paulo <paulo2022.dev@gmail.com>
2024-07-03 18:28:28 -0300

    Conta Banco
    novo commit

--- java/src/edu/paulo/contabanco/ContaTerminal.java
+++ java/src/edu/paulo/contabanco/ContaTerminal.java
@@ -1,8 +1,22 @@
 package edu.paulo.contabanco;
-
 import java.util.Scanner;
 
+/**
+ * @author Paulo Roberto
+ * @version 1.0
+ * @since 03/07/2024
+ */
+
 public class ContaTerminal {
+
+    /**
+     * @param NomeCliente parâmetro que armazena o nome do cliente
+     * @param agencia parâmetro que armazena o numero da agência do cliente
+     * @param numero parâmetro que armazena o numero da conta bancária do cliente
+     * @param saldo parâmetro que armazena o saldo da conta bancária do cliente
+     * @param args
+     */
+
     public static void main (String[] args){
 
         Scanner scanner = new Scanner(System.in);
@@ -23,7 +37,6 @@ public class ContaTerminal {
 
         System.out.println("Olá " + NomeCliente + "," + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", " + "conta " + numero + "," + "  seu saldo " + saldo + " já está dispónível para saque.");
         
-
         scanner.close();
     }
 }
